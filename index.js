f27b4feca54bd69d6fefe8bb7f24723a95b06c20
RichardEstime <richardjestime@yahoo.com>
2021-06-08 22:07:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+fetch('https://anapioficeandfire.com/api/books')
+.then(response => response.json())
+.then(json => console.log(json))
+// To pass the tests, don't forget to return your fetch!
   
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
